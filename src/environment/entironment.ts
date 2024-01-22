cba2d41e0193b16b73cd93b2d62bd2bd70bf27b1
Taras Shevchuk <tarashevchuk0401@gmail.com>
2024-01-22 19:41:24 +0100

    Materials, NgRx, Firebase installed

---
+++ src/environment/entironment.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDOCEijr1NxGnDYWPPhRS98-GLg7e8nO_0",
+    authDomain: "restaurant-app-af60c.firebaseapp.com",
+    projectId: "restaurant-app-af60c",
+    storageBucket: "restaurant-app-af60c.appspot.com",
+    messagingSenderId: "248625552131",
+    appId: "1:248625552131:web:ba1ed8d0250fd767016736",
+    measurementId: "G-49Z9VW6ZFZ"
+  };
